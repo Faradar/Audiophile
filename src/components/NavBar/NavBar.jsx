77233ad8aa467524570aed106ad2103eff4d11d3
Faradar <tomi_compuamste@hotmail.com>
2023-08-29 16:57:10 -0300

    Added react-router-bootstrap and LinkContainer to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,31 +3,35 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import CartWidget from "../CartWidget/CartWidget";
 import styles from "./NavBar.module.scss";
+import { LinkContainer } from "react-router-bootstrap";
 
 function NavBar() {
   return (
     <Navbar expand="lg" bg="dark" data-bs-theme="dark">
       <Container>
         {/* Brand */}
-        <Navbar.Brand href="/">
-          <img alt="Audiophile logo" src="/logo.svg"></img>
-        </Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>
+            <img alt="Audiophile logo" src="/logo.svg"></img>
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           {/* Links */}
           <Nav className="me-auto">
-            <Nav.Link href="/" className={styles.links}>
-              HOME
-            </Nav.Link>
-            <Nav.Link href="/category/headphones" className={styles.links}>
-              HEADPHONES
-            </Nav.Link>
-            <Nav.Link href="/category/speakers" className={styles.links}>
-              SPEAKERS
-            </Nav.Link>
-            <Nav.Link href="/category/earphones" className={styles.links}>
-              EARPHONES
-            </Nav.Link>
+            <LinkContainer to="/">
+              <Nav.Link className={styles.links}>HOME</Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/category/headphones">
+              <Nav.Link className={styles.links}>HEADPHONES</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/category/speakers">
+              <Nav.Link className={styles.links}>SPEAKERS</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/category/earphones">
+              <Nav.Link className={styles.links}>EARPHONES</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
         {/* CartWidget */}
